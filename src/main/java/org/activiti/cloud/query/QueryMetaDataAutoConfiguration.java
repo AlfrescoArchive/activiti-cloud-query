7559f464e43852c8cd27f0b796278ff88f73d74f
Salaboy <salaboy@gmail.com>
2018-04-07 10:29:08 +0100

    updating autoconfig

--- src/main/java/org/activiti/cloud/query/QueryMetaDataAutoConfiguration.java
+++ src/main/java/org/activiti/cloud/query/QueryMetaDataAutoConfiguration.java
@@ -3,9 +3,12 @@ package org.activiti.cloud.query;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import com.netflix.appinfo.ApplicationInfoManager;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,12 +16,25 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(ApplicationInfoManager.class)
 public class QueryMetaDataAutoConfiguration implements BeanClassLoaderAware {
 
-    // This code needs to live here until we find the right abstraction for registering/updating Service Metadata
+    @Value("${activiti.cloud.application.name:}")
+    private String applicationName;
+
     @Autowired
-    public QueryMetaDataAutoConfiguration(ApplicationInfoManager appInfoManager) {
+    private ApplicationInfoManager appInfoManager;
+
+    // This code needs to live here until we find the right abstraction for registering/updating Service Metadata
+
+    public QueryMetaDataAutoConfiguration() {
+
+    }
+
+    @PostConstruct
+    public void init(){
         Map<String, String> metadata = new HashMap<>();
         metadata.put("activiti-cloud-service-type",
                      "query");
+        metadata.put("activiti-cloud-application-name",
+                     applicationName);
         appInfoManager.registerAppMetadata(metadata);
     }
 
