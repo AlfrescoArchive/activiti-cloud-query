6d0f3388ddbd7b2296436e0a16c9bbcc0a43cde0
Oleksandr Merkulov <almerico@gmail.com>
2018-11-26 20:11:29 +0200

    update

--- src/main/java/org/activiti/cloud/query/QueryApplication.java
+++ src/main/java/org/activiti/cloud/query/QueryApplication.java
@@ -24,7 +24,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableActivitiQuery
 public class QueryApplication {
 
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
         SpringApplication.run(QueryApplication.class,
                 args);
         System.out.println("QueryApplication");
